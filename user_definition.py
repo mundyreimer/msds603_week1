da9f44b51cbb0dbe45a0da11e03b980ae2bbc0b3
mundyreimer <moreimer@dons.usfca.edu>
2020-04-04 13:01:44 -0700

    Initial add

---
+++ user_definition.py
@@ -0,0 +1,6 @@
+# https://developers.google.com/maps/documentation/javascript/get-api-key
+apikey = "AIzaSyB9ohXdz-pm_rH5JjgyT0w_t6HaKXQTUrU"
+orig_coord = '37.7909,-122.3925'
+dest_coord = '37.7765,-122.4506'
+output_file = 'time.tsv'
+bucket_name="msds694"
